72fc8b66d144ac1569dc3b662ce7715e1a1e4b05
JManuelLorenzo <juanma.lorenzo@correo.ucu.edu.uy>
2024-08-07 20:35:45 -0300

    manu resta

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,6 +3,8 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
+
+// Esta clase implementa la operación substaction>
 public class Substraction
 {
     public static int Substract(int a, int b)
